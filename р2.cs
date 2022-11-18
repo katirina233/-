7d86d63286bea0998117020157e956c12b11fb7a
katirina233 <118421008+katirina233@users.noreply.github.com>
2022-11-18 14:46:02 +0900

    Add files via upload

---
+++ р2.cs
@@ -0,0 +1,22 @@
+﻿Person aleks = new Person(); 
+
+string personName = aleks.name;
+int personAge = aleks.age;
+Console.WriteLine($"Имя: {personName}  Возраст {personAge}");  
+
+aleks.name = "Александр";
+aleks.age = 23;
+
+
+aleks.Print();   
+
+class Person
+{
+    public string name = "Матвей";
+    public int age=66;
+    public string book = "интересная книга в которой миллион страниц";
+    public void Print()
+    {
+        Console.WriteLine($"Имя: {name}  Возраст: {age} Книга:{book}");
+    }
+}
